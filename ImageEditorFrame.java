06e7de0e163893a31e8f24cdb3ecd76e6afa4cc7
unknown <luebze@gmail.com>
2015-05-07 11:43:13 +0200

    testbild

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -5,18 +5,30 @@ import java.awt.image.*;
 import java.io.*; 
 import javax.imageio.*; 
 
-public class ImageEditorFrame extends JFrame
+public class ImageEditorFrame extends JFrame 
 { 
 	public ImageEditorFrame()
 	{ 
+		ImageEditorPanel panel = new ImageEditorPanel(); 
 		setDefaultCloseOperation(EXIT_ON_CLOSE); 
 		setSize(400, 300); 
 		setVisible(true); 
 		setTitle("Leeres Fenster");
+		setDummyImage(panel);
+		add(panel);
 	} 
 	
 	public static void main(String[] args)
 	{
 		ImageEditorFrame frame = new ImageEditorFrame();
+		
+	}
+	private void setDummyImage(ImageEditorPanel panel) 
+	{ 
+		BufferedImage bufferedImage = new BufferedImage(400, 300, BufferedImage.TYPE_INT_RGB); 
+		Graphics g = bufferedImage.getGraphics(); 
+		g.setColor(Color.YELLOW); 
+		g.fillOval(10, 10, 380, 280); 
+		panel.setImage(bufferedImage); 
 	}
 }
